12f9bbe0c9816112885e6a6f0ceed7e7e91b7a59
Mauropieroni <mauropieroni@gmail.com>
2026-05-28 11:57:08 +0200

    Started to move things around, everything is broken atm

--- src/gwb_templates/utils.py
+++ src/gwb_templates/utils.py
@@ -1,6 +1,6 @@
 # Global imports
-from collections.abc import Callable, Sequence
-from typing import Any, Optional, TypeAlias
+from collections.abc import Callable
+from typing import Any, TypeAlias
 
 import os
 import jax
@@ -38,7 +38,7 @@ def check_paths(paths: Iterable[str]) -> None:
             os.makedirs(path)
 
 
-def gradient_automatic(
+def gradient_autodiff(
     function: IndexedDerivativeFn,
     frequency: Array,
     parameters: ArrayLike,
@@ -64,7 +64,7 @@ def gradient_automatic(
     return jax.jacfwd(function, argnums=1)(frequency, parameters, *args, **kwargs)
 
 
-def hessian_automatic(
+def hessian_autodiff(
     function: IndexedDerivativeFn,
     frequency: Array,
     parameters: ArrayLike,
@@ -93,79 +93,6 @@ def hessian_automatic(
     return jax.jacfwd(jax.jacfwd(function, argnums=1), argnums=1)(
         frequency, parameters, *args, **kwargs
     )
-
-
-class Signal_model(object):
-    """
-    Container for a signal template and its first/second derivatives.
-
-    """
-
-    def __init__(
-        self,
-        model_name: str,
-        template: TemplateFn,
-        dtemplate: Optional[IndexedDerivativeFn] = None,
-        d2template: Optional[IndexedDerivativeFn] = None,
-        model_label: Optional[str] = None,
-        parameter_names: Optional[Sequence[str]] = None,
-        parameter_labels: Optional[Sequence[str]] = None,
-        prior: Optional[dict[str, Any]] = None,
-    ) -> None:
-        """
-        Initialize a signal model wrapper.
-
-        Args:
-            model_name: Human-readable model identifier.
-            template: Base template function S(f, theta, ...).
-            dtemplate: Optional indexed first-derivative function.
-                If omitted, jacfwd(template) is used.
-            model_label: Optional formatted label for the model.
-            parameter_names: Optional ordered parameter names.
-            parameter_labels: Optional formatted labels for plots.
-            prior: Optional dictionary of parameter priors, with parameter names as
-                keys and prior specifications as values.
-        Returns:
-            None.
-        """
-
-        self.model_name = model_name
-        self.model_label = model_label if model_label is not None else model_name
-        self.parameter_names = (
-            list(parameter_names) if parameter_names is not None else []
-        )
-        self.parameter_labels = (
-            list(parameter_labels) if parameter_labels is not None else []
-        )
-
-        self.template = template
-
-        if dtemplate is not None:
-            self.dtemplate = dtemplate
-        else:
-            self.dtemplate = lambda *args, **kwargs: gradient_automatic(
-                template, *args, **kwargs
-            )
-
-        if d2template is not None:
-            self.d2template = d2template
-        else:
-            self.d2template = lambda *args, **kwargs: hessian_automatic(
-                template, *args, **kwargs
-            )
-
-        self.prior = prior if prior is not None else {}
-
-    @property
-    def Nparams(self) -> int:
-        """
-        Return the number of parameters in the model.
-
-        Returns:
-            Number of parameters.
-        """
-
-        return len(self.parameter_names)
 
 
 def make_log_log_interpolator(
